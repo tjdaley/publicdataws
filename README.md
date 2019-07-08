45e17c6d24209615040f90d7eacf50a4d0e4916a
Thomas J. Daley, J.D <tjd@powerdaley.com>
2019-07-08 18:59:59 -0500

    First git push

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # publicdataws
 Public Data Web Service
+
+*Web service that logins into and queries Public Data servers.*
+
+# Service Provider API Documentation
+
+Public Data's API is documented here: http://www.publicdata.com/pdapidocs/
---
+++ app/util/publicdata.py
@@ -0,0 +1,9 @@
+"""
+publicdata.py - Python API for Public Data searches.
+
+Based on documentation available at: http://www.publicdata.com/pdapidocs/index.php
+
+Copyright (c) 2019 by Thomas J. Daley, J.D. All Rights Reserved.
+"""
+__author__ = "Thomas J. Daley, J.D."
+__version__ = "0.0.1"
