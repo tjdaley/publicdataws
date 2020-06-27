c66ee4a0c4c03bc1a9f6dd498e1e5fadcc7cb3b4
Tom Daley <tjd@powerdaley.com>
2020-06-27 12:46:44 -0500

    perform lint cleanup

--- app/views/login/forms/RegisterForm.py
+++ app/views/login/forms/RegisterForm.py
@@ -1,5 +1,6 @@
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 
+
 class RegisterForm(Form):
     name = StringField("Name", [validators.DataRequired(), validators.Length(min=1, max=50)])
     email = StringField("Email", [validators.Length(min=6, max=50)])
